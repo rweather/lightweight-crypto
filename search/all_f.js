005064cabe5bacc04cc2b11dfcc895cc7efa92bf
Rhys Weatherley <rhys.weatherley@gmail.com>
2020-01-03 13:45:43 +1000

    Update docs

--- search/all_f.js
+++ search/all_f.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['tag_5flen',['tag_len',['../structaead__cipher__t.html#a6f95e595bfbe7657965475d0ebdcaada',1,'aead_cipher_t']]],
-  ['tk1',['TK1',['../structskinny__128__384__key__schedule__t.html#ae593a4298c2e6905a2a7c6a9ff0a90ee',1,'skinny_128_384_key_schedule_t::TK1()'],['../structskinny__128__256__key__schedule__t.html#aad0cfbe4d29908c8d22a8b6a44fde410',1,'skinny_128_256_key_schedule_t::TK1()']]]
+  ['tk1',['TK1',['../structskinny__128__384__key__schedule__t.html#ad4e48c2306fa72a2a3e9ac0cb3cfd041',1,'skinny_128_384_key_schedule_t::TK1()'],['../structskinny__128__256__key__schedule__t.html#ab1e0f45c5fd2ec825d74cdf5bf405e7e',1,'skinny_128_256_key_schedule_t::TK1()']]]
 ];
