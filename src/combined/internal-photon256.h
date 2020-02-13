0805cfe373b70e5c0cfad58fbe419f99fba3f4ca
Rhys Weatherley <rhys.weatherley@gmail.com>
2020-02-13 19:15:50 +1000

    PHOTON-256 permutation

--- src/combined/internal-photon256.h
+++ src/combined/internal-photon256.h
@@ -20,38 +20,35 @@
  * DEALINGS IN THE SOFTWARE.
  */
 
-#include "test-cipher.h"
+#ifndef LW_INTERNAL_PHOTON256_H
+#define LW_INTERNAL_PHOTON256_H
 
-void test_chachapoly(void);
-void test_cham(void);
-void test_clyde128(void);
-void test_gift128(void);
-void test_gift64(void);
-void test_gimli24(void);
-void test_keccak(void);
-void test_knot(void);
-void test_photon256(void);
-void test_pyjamask(void);
-void test_shadow(void);
-void test_simp(void);
-void test_skinny128(void);
-void test_sliscp_light(void);
+/**
+ * \file internal-photon256.h
+ * \brief Internal implementation of the PHOTON-256 permutation.
+ *
+ * Warning: The current implementation of PHOTON-256 is constant-time
+ * but not constant-cache.
+ */
 
-int main(int argc, char *argv[])
-{
-    test_chachapoly();
-    test_cham();
-    test_clyde128();
-    test_gift128();
-    test_gift64();
-    test_gimli24();
-    test_keccak();
-    test_knot();
-    test_photon256();
-    test_pyjamask();
-    test_shadow();
-    test_simp();
-    test_skinny128();
-    test_sliscp_light();
-    return test_exit_result;
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * \brief Size of the PHOTON-256 permutation state in bytes.
+ */
+#define PHOTON256_STATE_SIZE 32
+
+/**
+ * \brief Permutes the PHOTON-256 state.
+ *
+ * \param state The state to be permuted.
+ */
+void photon256_permute(unsigned char state[PHOTON256_STATE_SIZE]);
+
+#ifdef __cplusplus
 }
+#endif
+
+#endif
--- test/unit/test-photon256.c
+++ test/unit/test-photon256.c
@@ -20,38 +20,41 @@
  * DEALINGS IN THE SOFTWARE.
  */
 
+#include "internal-photon256.h"
 #include "test-cipher.h"
+#include <stdio.h>
+#include <string.h>
 
-void test_chachapoly(void);
-void test_cham(void);
-void test_clyde128(void);
-void test_gift128(void);
-void test_gift64(void);
-void test_gimli24(void);
-void test_keccak(void);
-void test_knot(void);
-void test_photon256(void);
-void test_pyjamask(void);
-void test_shadow(void);
-void test_simp(void);
-void test_skinny128(void);
-void test_sliscp_light(void);
+/* Test vector for PHOTON-256 generated with the PHOTON-Beetle reference code */
+static unsigned char const photon256_input[] = {
+    0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07,
+    0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f,
+    0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17,
+    0x18, 0x19, 0x1a, 0x1b, 0x1c, 0x1d, 0x1e, 0x1f
+};
+static unsigned char const photon256_output[] = {
+    0x25, 0x5e, 0x27, 0x0d, 0x37, 0xe9, 0x0d, 0x76,
+    0xbc, 0xa8, 0x38, 0x53, 0x65, 0xba, 0xae, 0x7d,
+    0x4a, 0xcc, 0x71, 0x33, 0x8f, 0x26, 0x5b, 0x0c,
+    0x1b, 0x52, 0x09, 0x3f, 0x4d, 0x48, 0xee, 0xf9
+};
 
-int main(int argc, char *argv[])
+void test_photon256(void)
 {
-    test_chachapoly();
-    test_cham();
-    test_clyde128();
-    test_gift128();
-    test_gift64();
-    test_gimli24();
-    test_keccak();
-    test_knot();
-    test_photon256();
-    test_pyjamask();
-    test_shadow();
-    test_simp();
-    test_skinny128();
-    test_sliscp_light();
-    return test_exit_result;
+    unsigned char state[32];
+
+    printf("PHOTON-256 Permutation:\n");
+
+    printf("    Test Vector ... ");
+    fflush(stdout);
+    memcpy(state, photon256_input, sizeof(photon256_input));
+    photon256_permute(state);
+    if (!test_memcmp(state, photon256_output, sizeof(photon256_output))) {
+        printf("ok\n");
+    } else {
+        printf("failed\n");
+        test_exit_result = 1;
+    }
+
+    printf("\n");
 }
