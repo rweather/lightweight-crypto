70f5ce35e87e118481e4acc7529bef79817ff318
Rhys Weatherley <rhys.weatherley@gmail.com>
2020-03-14 12:37:55 +1000

    Update docs

--- search/classes_8.js
+++ search/classes_8.js
@@ -1,6 +1,8 @@
 var searchData=
 [
   ['saturnin_5fhash_5fstate_5ft',['saturnin_hash_state_t',['../unionsaturnin__hash__state__t.html',1,'']]],
+  ['shadow384_5fstate_5ft',['shadow384_state_t',['../unionshadow384__state__t.html',1,'']]],
+  ['shadow512_5fstate_5ft',['shadow512_state_t',['../unionshadow512__state__t.html',1,'']]],
   ['skinny_5f128_5f128_5fkey_5fschedule_5ft',['skinny_128_128_key_schedule_t',['../structskinny__128__128__key__schedule__t.html',1,'']]],
   ['skinny_5f128_5f256_5fkey_5fschedule_5ft',['skinny_128_256_key_schedule_t',['../structskinny__128__256__key__schedule__t.html',1,'']]],
   ['skinny_5f128_5f384_5fkey_5fschedule_5ft',['skinny_128_384_key_schedule_t',['../structskinny__128__384__key__schedule__t.html',1,'']]],
