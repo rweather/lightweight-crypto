dc3157c2b0c9fb45ae12f5ec7e3091cc7acd1437
Rhys Weatherley <rhys.weatherley@gmail.com>
2020-04-25 13:47:52 +1000

    Update docs

--- search/all_8.js
+++ search/all_8.js
@@ -7,6 +7,7 @@ var searchData=
   ['internal_2dcham_2eh',['internal-cham.h',['../internal-cham_8h.html',1,'']]],
   ['internal_2ddrysponge_2eh',['internal-drysponge.h',['../internal-drysponge_8h.html',1,'']]],
   ['internal_2dforkskinny_2eh',['internal-forkskinny.h',['../internal-forkskinny_8h.html',1,'']]],
+  ['internal_2dgift128_2dconfig_2eh',['internal-gift128-config.h',['../internal-gift128-config_8h.html',1,'']]],
   ['internal_2dgift128_2eh',['internal-gift128.h',['../internal-gift128_8h.html',1,'']]],
   ['internal_2dgift64_2eh',['internal-gift64.h',['../internal-gift64_8h.html',1,'']]],
   ['internal_2dgimli24_2eh',['internal-gimli24.h',['../internal-gimli24_8h.html',1,'']]],
