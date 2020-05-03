66ab5e61034ffa09972d7f4033e5dd83250c7201
Rhys Weatherley <rhys.weatherley@gmail.com>
2020-05-03 12:48:52 +1000

    Update docs

--- search/classes_8.js
+++ search/classes_8.js
@@ -1,6 +1,8 @@
 var searchData=
 [
   ['poly1305_5fstate_5ft',['poly1305_state_t',['../structpoly1305__state__t.html',1,'']]],
-  ['pyjamask_5fkey_5fschedule_5ft',['pyjamask_key_schedule_t',['../structpyjamask__key__schedule__t.html',1,'']]],
-  ['pyjamask_5fmasked_5fkey_5fschedule_5ft',['pyjamask_masked_key_schedule_t',['../structpyjamask__masked__key__schedule__t.html',1,'']]]
+  ['pyjamask_5f128_5fkey_5fschedule_5ft',['pyjamask_128_key_schedule_t',['../structpyjamask__128__key__schedule__t.html',1,'']]],
+  ['pyjamask_5f96_5fkey_5fschedule_5ft',['pyjamask_96_key_schedule_t',['../structpyjamask__96__key__schedule__t.html',1,'']]],
+  ['pyjamask_5fmasked_5f128_5fkey_5fschedule_5ft',['pyjamask_masked_128_key_schedule_t',['../structpyjamask__masked__128__key__schedule__t.html',1,'']]],
+  ['pyjamask_5fmasked_5f96_5fkey_5fschedule_5ft',['pyjamask_masked_96_key_schedule_t',['../structpyjamask__masked__96__key__schedule__t.html',1,'']]]
 ];
