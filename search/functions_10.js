e1b83f44a1cf652ced187c77ac52bfdb813f0b65
Rhys Weatherley <rhys.weatherley@gmail.com>
2020-06-08 04:46:11 +1000

    Update docs

--- search/functions_10.js
+++ search/functions_10.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['wage_5fabsorb',['wage_absorb',['../internal-wage_8h.html#a92bb87664e901cc2d4b0f9eb7ecaa3e4',1,'internal-wage.c']]],
+  ['wage_5fabsorb',['wage_absorb',['../internal-wage_8h.html#a7c4ed5ee473750c346be5d991da439ca',1,'internal-wage.c']]],
   ['wage_5fabsorb_5fkey',['wage_absorb_key',['../internal-wage_8h.html#a37c149d724e71d78f0f04ed38ea2ba53',1,'internal-wage.c']]],
   ['wage_5faead_5fdecrypt',['wage_aead_decrypt',['../wage_8h.html#a7208be6f038e1eaaf533a19a360b0eba',1,'wage.c']]],
   ['wage_5faead_5fencrypt',['wage_aead_encrypt',['../wage_8h.html#af3d1936e9fe622ca3e7c2102674a409c',1,'wage.c']]],
@@ -8,5 +8,5 @@ var searchData=
   ['wage_5fget_5frate',['wage_get_rate',['../internal-wage_8h.html#a07ae5028c4288298a7f60ea06a067792',1,'internal-wage.c']]],
   ['wage_5finit',['wage_init',['../internal-wage_8h.html#a4362d34d0dc51d5d5ee21f97b83782c5',1,'internal-wage.c']]],
   ['wage_5fpermute',['wage_permute',['../internal-wage_8h.html#a4d179c998d56f1d8f500ce386f4d1459',1,'internal-wage.c']]],
-  ['wage_5fset_5frate',['wage_set_rate',['../internal-wage_8h.html#a9183b8045ee9ee4d5e2436a29fb75456',1,'internal-wage.c']]]
+  ['wage_5fset_5frate',['wage_set_rate',['../internal-wage_8h.html#a7b78baf1d156a3a6b400a62e7255cab8',1,'internal-wage.c']]]
 ];
