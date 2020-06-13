8f4c4795c1bf34780e0fee2c32f843afed6636bc
Rhys Weatherley <rhys.weatherley@gmail.com>
2020-06-13 17:58:11 +1000

    Update docs

--- search/functions_6.js
+++ search/functions_6.js
@@ -1,5 +1,7 @@
 var searchData=
 [
-  ['hyena_5faead_5fdecrypt',['hyena_aead_decrypt',['../hyena_8h.html#a26da9f2a20319ba96f300023dee70895',1,'hyena.c']]],
-  ['hyena_5faead_5fencrypt',['hyena_aead_encrypt',['../hyena_8h.html#a5bb8ffb382760991eb77e16b65292481',1,'hyena.c']]]
+  ['hyena_5fv1_5faead_5fdecrypt',['hyena_v1_aead_decrypt',['../hyena_8h.html#a01ef413a743bb311860c46160a0714a8',1,'hyena.c']]],
+  ['hyena_5fv1_5faead_5fencrypt',['hyena_v1_aead_encrypt',['../hyena_8h.html#a73a7a0d7997796d1cc173e54b3398d15',1,'hyena.c']]],
+  ['hyena_5fv2_5faead_5fdecrypt',['hyena_v2_aead_decrypt',['../hyena_8h.html#af1a5e38c025a8f20d75e89fdfdcbec96',1,'hyena.c']]],
+  ['hyena_5fv2_5faead_5fencrypt',['hyena_v2_aead_encrypt',['../hyena_8h.html#ab9145bd4170880af918ba1ccf713ef07',1,'hyena.c']]]
 ];
