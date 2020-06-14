18ead16dcf1f6c1b8148476753751062be1823a2
Rhys Weatherley <rhys.weatherley@gmail.com>
2020-06-14 19:29:54 +1000

    Update docs

--- search/functions_1.js
+++ search/functions_1.js
@@ -2,8 +2,8 @@ var searchData=
 [
   ['cham128_5f128_5fencrypt',['cham128_128_encrypt',['../internal-cham_8h.html#aad42dd29c32e10cdc7bcc763fbd40d2e',1,'internal-cham.c']]],
   ['cham64_5f128_5fencrypt',['cham64_128_encrypt',['../internal-cham_8h.html#a40c0a3f8912d4dbb68420ba1738213ce',1,'internal-cham.c']]],
-  ['clyde128_5fdecrypt',['clyde128_decrypt',['../internal-spook_8h.html#a526175087e2e5ddb05854bbbd116a3ba',1,'internal-spook.c']]],
-  ['clyde128_5fencrypt',['clyde128_encrypt',['../internal-spook_8h.html#a8ba22701c790c33f46aa984758c61c7d',1,'internal-spook.c']]],
+  ['clyde128_5fdecrypt',['clyde128_decrypt',['../internal-spook_8h.html#a09faff95cea090fb3c226f12fd06fdc3',1,'internal-spook.c']]],
+  ['clyde128_5fencrypt',['clyde128_encrypt',['../internal-spook_8h.html#af2f9cfc7a45bee96db20ffedfb6e358f',1,'internal-spook.c']]],
   ['comet_5f128_5fcham_5faead_5fdecrypt',['comet_128_cham_aead_decrypt',['../comet_8h.html#aaa3905845fb7f9f8abbff2d1f2211693',1,'comet.c']]],
   ['comet_5f128_5fcham_5faead_5fencrypt',['comet_128_cham_aead_encrypt',['../comet_8h.html#a619af0f5d8e799fdad2e03aedee620e7',1,'comet.c']]],
   ['comet_5f64_5fcham_5faead_5fdecrypt',['comet_64_cham_aead_decrypt',['../comet_8h.html#ae4dff2dff95cee37ce1647651e642bf5',1,'comet.c']]],
