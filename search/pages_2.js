400030f3852779bae60eaf17b0e1ac2a73565b64
Rhys Weatherley <rhys.weatherley@gmail.com>
2020-07-27 17:09:20 +1000

    Update docs

--- search/pages_2.js
+++ search/pages_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['performance',['Performance',['../performance.html',1,'']]],
-  ['performance_20on_20avr',['Performance on AVR',['../performance_avr.html',1,'']]]
+  ['performance_20on_20avr',['Performance on AVR',['../performance_avr.html',1,'']]],
+  ['performance_20of_20masked_20algorithms',['Performance of Masked Algorithms',['../performance_masking.html',1,'']]]
 ];
