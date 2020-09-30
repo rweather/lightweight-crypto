48f1a4d1ac41d47f59c8ab0f1299aa3454c0d14f
Rhys Weatherley <rhys.weatherley@gmail.com>
2020-10-01 07:56:02 +1000

    Update docs

--- search/all_10.js
+++ search/all_10.js
@@ -4,6 +4,7 @@ var searchData=
   ['performance_2edox',['performance.dox',['../performance_8dox.html',1,'']]],
   ['performance_20on_20avr',['Performance on AVR',['../performance_avr.html',1,'']]],
   ['performance_5favr_2edox',['performance_avr.dox',['../performance__avr_8dox.html',1,'']]],
+  ['performance_20on_2032_2dbit_20platforms_20_28baseline_29',['Performance on 32-bit platforms (baseline)',['../performance_baseline.html',1,'']]],
   ['performance_20of_20masked_20algorithms',['Performance of Masked Algorithms',['../performance_masking.html',1,'']]],
   ['performance_5fmasking_2edox',['performance_masking.dox',['../performance__masking_8dox.html',1,'']]],
   ['phase_202_20performance_20figures',['Phase 2 Performance Figures',['../performance_phase2.html',1,'']]],
--- search/pages_2.js
+++ search/pages_2.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['performance_20on_2032_2dbit_20platforms',['Performance on 32-bit platforms',['../performance.html',1,'']]],
   ['performance_20on_20avr',['Performance on AVR',['../performance_avr.html',1,'']]],
+  ['performance_20on_2032_2dbit_20platforms_20_28baseline_29',['Performance on 32-bit platforms (baseline)',['../performance_baseline.html',1,'']]],
   ['performance_20of_20masked_20algorithms',['Performance of Masked Algorithms',['../performance_masking.html',1,'']]],
   ['phase_202_20performance_20figures',['Phase 2 Performance Figures',['../performance_phase2.html',1,'']]]
 ];
